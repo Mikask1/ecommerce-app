5ae729b7c3b1526b14c560e51876bc650f2d71de
Mikask <darrenprasetya40@gmail.com>
2023-10-15 04:06:09 +0700

    feat: add phone number to user object

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,6 +17,7 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['string', 'max:255'],
+            'phone_number' => ['string', 'regex:/^[0-9\s+\(\)-]+$/'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
